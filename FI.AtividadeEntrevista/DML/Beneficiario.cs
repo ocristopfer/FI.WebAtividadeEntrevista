4c57e8beb611c3ed7b815f1d243ff91756ef00ee
Cristopfer Luis <ocristopfer@gmail.com>
2020-10-02 13:32:58 -0300

    Adicionar arquivos de projeto.

---
+++ FI.AtividadeEntrevista/DML/Beneficiario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.DML
+{
+    /// <summary>
+    /// Classe de cliente que representa o registo na tabela Cliente do Banco de Dados
+    /// </summary>
+    public class Beneficiario
+    {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// CPF
+        /// </summary>
+        public string CPF { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// idCliente
+        /// </summary>
+        public long idCliente { get; set; }
+
+    }
+}
